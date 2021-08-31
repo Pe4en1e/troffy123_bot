822c43115a6424164cfb5c22b1afb45b22d85f1a
Roberts Vesīks <alex36254@gmail.com>
2021-08-31 18:34:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -258,48 +258,6 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
-  var timer = new Date().getHours();
-  if(timer = 19) {
-    client.channels.cache.get('694326993247797319').send('До сентября 5 часов')
-  }
-})
-
-client.on('message', message => {
-  var timer = new Date().getHours();
-  if(timer = 20) {
-    client.channels.cache.get('694326993247797319').send('До сентября 4 часа')
-  }
-})
-
-client.on('message', message => {
-  var timer = new Date().getHours();
-  if(timer = 21) {
-    client.channels.cache.get('694326993247797319').send('До сентября 3 часа')
-  }
-})
-
-client.on('message', message => {
-  var timer = new Date().getHours();
-  if(timer = 22) {
-    client.channels.cache.get('694326993247797319').send('До сентября 2 часа')
-  }
-})
-
-client.on('message', message => {
-  var timer = new Date().getHours();
-  if(timer = 23) {
-    client.channels.cache.get('694326993247797319').send('Сентябрь через час!')
-  }
-})
-
-client.on('message', message => {
-  var timer = new Date().getHours();
-  if(timer = 0) {
-    client.channels.cache.get('694326993247797319').send('@everyone лето проебано!')
-  }
-})
-
 client.on('ready', () => {
   client.channels.cache.get('694326993247797319').send('троффи пришел отравлять жизнь')
 })
